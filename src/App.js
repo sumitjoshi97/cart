3e7ec7092987c9c8e416ded3e66dfc9192b3cccd
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-08-12 23:41:55 +0530

    refactored

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Filter from './containers/Filter/Filter';
+import Cart from './components/Cart/Cart'
+import Home from './containers/Home/Home'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <Filter/> */}
+        <Home/>
+        <Cart/>
       </div>
     );
   }
