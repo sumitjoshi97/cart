4c74438458e589e4f5a2482a2bc45d58e4705095
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-08-12 23:43:27 +0530

    action types added

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,6 @@
+export const SET_PRODUCTS = 'SET_PRODUCTS';
+export const SET_PRODUCTS_FAIL = 'SET_PRODUCTS_FAIL';
+
+export const ADD_PRODUCT_TO_CART = 'ADD_PRODUCT_TO_CART'
+
+export const SET_FILTER = 'SET_FILTER'
