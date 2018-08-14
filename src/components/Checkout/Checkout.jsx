7892247d31bfde429873903f57f380fdc2ae6219
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-08-14 13:08:00 +0530

    checkout component added

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,23 +1,40 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import CheckoutSummary from './CheckoutSummary/CheckoutSummary'
+// import CartItem from '../Home/Cart/Cartitem/CartItem'
+import ContactData from './ContactData/ContactData'
 import './Checkout.css'
 
 export class Checkout extends Component {
   render() {
+    let summary = null;
+    if (this.props.cart) {
+      summary = this.props.cart.map(product => {
+          return <CheckoutSummary
+            key={product.id}
+            title={product.title}
+            image={product.image_url}
+            quantity={product.quantity} />
+        })
+    }
+
     return (
       <div className="checkout">
         <h2>Checkout</h2>
-        {/* {this.props.cart.totalPrice} */}
-        {console.log(this.props.cart.totalPrice)}
+        {/* <CheckoutSummary cart={this.props.cart}/> */}
+        <div className="checkout__summary">
+          {summary}
+        </div>
+        
+        {this.props.totalPrice}
+        <ContactData/> {console.log(this.props.totalPrice)}
       </div>
     )
   }
 }
 
 const mapStateToProps = state => {
-  return {
-    cart: state.cart.cart
-  }
+  return {cart: state.cart.cart, totalPrice: state.cart.totalPrice}
 }
 
 export default connect(mapStateToProps, null)(Checkout)
