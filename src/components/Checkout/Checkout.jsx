05eade0e41134247ccaf80997818e1ea0fdb948b
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-08-15 13:03:49 +0530

    redirect added

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,40 +1,47 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {Route, Redirect} from 'react-router-dom'
 import CheckoutSummary from './CheckoutSummary/CheckoutSummary'
-// import CartItem from '../Home/Cart/Cartitem/CartItem'
 import ContactData from './ContactData/ContactData'
 import './Checkout.css'
 
 export class Checkout extends Component {
+  checkoutCancelHandler = () => {
+    this.props.history.goBack()
+  }
+  checkoutContinueHandler = () => {
+    this.props.history.replace('/checkout/contact-data')
+  }
+
   render() {
-    let summary = null;
+    let summary = <Redirect to="/"/>
+
     if (this.props.cart) {
-      summary = this.props.cart.map(product => {
-          return <CheckoutSummary
-            key={product.id}
-            title={product.title}
-            image={product.image_url}
-            quantity={product.quantity} />
-        })
+      const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null 
+      
+      summary = (
+        <div className="checkout">
+        {purchasedRedirect}
+          <CheckoutSummary
+            cart={this.props.cart}
+            totalPrice={this.props.totalPrice}
+            checkoutCancelled={this.checkoutCancelHandler}
+            checkoutContinued={this.checkoutContinueHandler} />
+          <Route path={this.props.match.path + '/contact-data'} component={ContactData}/>
+        </div>
+      )
     }
 
-    return (
-      <div className="checkout">
-        <h2>Checkout</h2>
-        {/* <CheckoutSummary cart={this.props.cart}/> */}
-        <div className="checkout__summary">
-          {summary}
-        </div>
-        
-        {this.props.totalPrice}
-        <ContactData/> {console.log(this.props.totalPrice)}
-      </div>
-    )
+    return summary
   }
 }
 
 const mapStateToProps = state => {
-  return {cart: state.cart.cart, totalPrice: state.cart.totalPrice}
+  return {
+    cart: state.cart.cart, 
+    totalPrice: state.cart.totalPrice,
+    purchased: state.order.purchased
+  }
 }
 
 export default connect(mapStateToProps, null)(Checkout)
