5f0c7baf5b7ba047305f77fe38931b50c1729e41
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-08-16 14:30:09 +0530

    added price to cart state

--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -2,6 +2,7 @@ import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
     cart: [],
+    totalPrice: 0,
     isCartActive: false
 }
 
@@ -9,7 +10,7 @@ const initialState = {
 const addProductToCart = (state, action) => {
     let product = action.product;
     let cart = [...state.cart]
-
+    
     // if product is in state
     let tempProduct = state.cart.find(p => p.id === product.id)
     if (tempProduct) {
@@ -22,15 +23,17 @@ const addProductToCart = (state, action) => {
                     ...state.cart[index],
                     quantity: quantity
                 }])
-                .concat(state.cart.slice(index + 1))
+                .concat(state.cart.slice(index + 1)),
+            totalPrice: state.totalPrice + product.price
         }
     }
 
-    // if product is not in state , store product with setting quantity = 1
+    // if product is not in state , store product with quantity = 1
     else {
         product.quantity = 1;
         return { ...state,
-            cart: cart.concat(product)
+            cart: cart.concat(product),
+            totalPrice: state.totalPrice + product.price
         }
     }
 }
