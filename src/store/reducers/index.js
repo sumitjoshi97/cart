e6def364a894c4df5d25b72c36b8cbb59601debd
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-08-16 14:31:24 +0530

    added exports

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,12 +1,15 @@
 import {combineReducers} from 'redux';
 
+import filterReducer from './filter';
 import cartReducer from './cart';
-// import filterReducer from './filter';
+import orderReducer from './order';
 import productReducer from './product'
 
 const reducer = combineReducers({
     cart: cartReducer,
-    product: productReducer
+    product: productReducer,
+    filter: filterReducer,
+    order: orderReducer
 })
 
 export default reducer
