3818bb1020e7d9548395ae22a9c734ad46f7b5d6
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-08-16 14:27:13 +0530

    typos

--- src/components/Checkout/CheckoutSummary/CheckoutSummary.jsx
+++ src/components/Checkout/CheckoutSummary/CheckoutSummary.jsx
@@ -7,8 +7,8 @@ const CheckoutSummary = props => {
   if (props.cart) {
     products = props.cart.map(product => {
       return (
-        <div className="checkout-summary__product">
-          <img src={product.image} alt="order" className="checkout-summary__product__image" />
+        <div className="checkout-summary__product" key={product.id}>
+          <img src={product.image_url} alt="order" className="checkout-summary__product__image" />
           <div className="checkout-summary__product__info">
             <div className="checkout-summary__product__info__title">{product.title}</div>
             <div className="checkout-summary__product__info__quantity">x {product.quantity}</div>
@@ -22,13 +22,16 @@ const CheckoutSummary = props => {
       <div className="checkout-summary__list">
         {products}
       </div>
-      <div className="checkout-summary__price">Total Price: {props.totalPrice.toFixed(2)}</div>
-      <button 
-        className="checkout-summary__cancel-btn"
-        onClick={props.checkoutCancelled}>CANCEL</button>
-      <button 
-        className="checkout-summary__continue-btn"
-        onClick={props.checkoutContinued}>CONTINUE</button> 
+      <h2 className="checkout-summary__price">Total Price: USD {props.totalPrice.toFixed(2)}</h2>
+      <div className="checkout-summary__btn">
+        <button
+          className="checkout-summary__btn--cancel btn-large"
+          onClick={props.checkoutCancelled}>CANCEL</button>
+        <button
+          className="checkout-summary__btn--continue btn-large"
+          onClick={props.checkoutContinued}>CONTINUE</button>
+      </div>
+
     </div>
   )
 }
