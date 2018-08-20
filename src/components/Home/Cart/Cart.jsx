16f82faa5452f05f966eea94ee9960d331157df9
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-08-20 16:52:06 +0530

    handle click mdified

--- src/components/Home/Cart/Cart.jsx
+++ src/components/Home/Cart/Cart.jsx
@@ -7,12 +7,31 @@ import * as actions from '../../../store/actions/index';
 import {withRouter} from 'react-router-dom'
 
 export class Cart extends Component {
+    
+    componentDidMount() {
+        if (this.props.purchased) {
+            this.props.onCartInit()
+        }
+    }
+
     handleClick = () => {
-        this.props.history.push('/checkout');
+        this.props.onPurchaseInit()
+        if (!this.props.auth && this.props.cart.length>0) {
+            this.props.onCartCheckout()
+            this.props.onSetAuthRedirectPath('/checkout')
+            this.props.history.push('/auth');
+        }
+        else if(this.props.auth && this.props.cart.length>0) {
+            this.props.onCartCheckout()
+            this.props.history.push('/checkout');
+        }
+        else {
+            //
+        }
     }
 
     render() {
-        const {isCartActive} = this.props;
+        const { isCartActive } = this.props;
 
         let cartList = [];
         if (this.props.cart) {
@@ -38,7 +57,9 @@ export class Cart extends Component {
                     {cartList}
                 </ul>
                 <div className="cart__checkout">
-                    <button className="cart__checkout--btn btn-large" onPointerDown={this.handleClick}>Checkout</button>
+                    <button 
+                        className="cart__checkout--btn btn-large" 
+                        onPointerDown={this.handleClick}>Checkout</button>
                 </div>
             </div>
         )
@@ -49,13 +70,21 @@ const mapStateToProps = state => {
     return {
         cart: state.cart.cart,
         isCartActive: state.cart.isCartActive,
-        totalPrice: state.cart.totalPrice
+        totalPrice: state.cart.totalPrice,
+        checkout: state.cart.checkout,
+        purchased: state.order.purchased,
+        isAuth: state.auth.token !== null,
+        authRedirectPath: state.auth.authRedirectPath
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onToggleCart: () => dispatch(actions.toggleCart())
+        onToggleCart: () => dispatch(actions.toggleCart()),
+        onPurchaseInit: () => dispatch(actions.purchaseInit()),
+        onCartInit: () => dispatch(actions.cartInit()),
+        onCartCheckout: () => dispatch(actions.cartCheckout()),
+        onSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path))
     }
 }
 
