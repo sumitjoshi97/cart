7867983985cb1f3292a9c8a6d0803aee34eeccac
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-08-27 23:47:16 +0530

    spinner added for loading state

--- src/components/Home/Products/Products.jsx
+++ src/components/Home/Products/Products.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import * as actions from '../../../store/actions/index';
 import Product from './Product/Product';
+import Spinner from '../../UI/Spinner/Spinner'
 import "./Products.css";
 
 export class Home extends Component {
@@ -10,8 +11,9 @@ export class Home extends Component {
   }
   
   render() {
-    let productsList = [];
+    let productsList = null;
     let {products, filters} = this.props
+
     if (products) {
       if (filters.length > 0) {
         products = products.filter(product => filters.find(filter => product.availableSizes.find(size => size === filter)))
@@ -22,6 +24,9 @@ export class Home extends Component {
           product={product}
         />
       })
+    }
+    if (this.props.loading) {
+      productsList = <Spinner />
     }
     
     return (
@@ -36,6 +41,7 @@ export class Home extends Component {
 
 const mapStateToProps = state => {
   return {
+    loading: state.product.loading,
     products: state.product.products,
     cart: state.cart.cart,
     filters: state.filter.filters
