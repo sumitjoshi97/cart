c3051710c83342e48c31aa5fcb36397f2115828c
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-08-28 18:20:11 +0530

    converted to functional component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home/Home'
 import Checkout from './components/Checkout/Checkout'
 import Sidebar from './components/SideBar/Sidebar';
 import Auth from './components/Auth/Auth'
 import Logout from './components/Auth/Logout/Logout'
 import Orders from './components/Orders/Orders'
-class App extends Component {
-  render() {
-    const right = {
-      marginLeft: '32rem'
-    }
-    return (
-      <Router>
-        <div>
-          <Sidebar/>
-          <div style={right}>
-            <Switch>
-              <Route path='/auth' component={Auth}/>
-              <Route path='/logout' component={Logout}/>
-              <Route path='/orders' component={Orders}/>
-              <Route path='/checkout' component={Checkout}/>
-              <Route path='/' component={Home}/>
-            </Switch>
-          </div>
+import './App.css'
+const App = () => (
+  <Router>
+    <div className="app">
+      <Sidebar/>
+      <div>
+        <Switch>
+          <Route path='/auth' component={Auth}/>
+          <Route path='/logout' component={Logout}/>
+          <Route path='/orders' component={Orders}/>
+          <Route path='/checkout' component={Checkout}/>
+          <Route path='/' component={Home}/>
+        </Switch>
+      </div>
+    </div>
 
-        </div>
-
-      </Router>
-    );
-  }
-}
+  </Router>
+)
 
 export default App;
