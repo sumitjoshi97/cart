d36b1326b59e5a30c0caa0b14aa29a5f962a5a15
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-08-28 18:18:56 +0530

    modified for phone displays

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, {Component} from 'react'
+import classNames from 'classnames';
 import Navigation from './Navigation/Navigation'
-import Filter from './Filter/Filter' 
+import Filter from './Filter/Filter'
 import './Sidebar.css'
-const Sidebar = props => {
-  return (
-    <div className="sidebar">
-        <Navigation/>
-        <div className="sidebar__divider"></div>
-        <Filter/>
-    </div>
-  )
-}
+class Sidebar extends Component {
+  state = {
+    isSidebarOpen: false
+  }
+
+  onToggleSidebar = () => {
+    let {isSidebarOpen} = this.state
+    this.setState({ isSidebarOpen: !isSidebarOpen })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="sidebar">
+          <Navigation/>
+          <div className="sidebar__divider"></div>
+          <Filter/>
+        </div>
+
+        <div 
+          className={classNames("sidebar sidebar__toggle", {"sidebar__toggle-open": this.state.isSidebarOpen})} >
+          <button className="sidebar__toggle__btn" onPointerDown={this.onToggleSidebar}>+</button>
+          <Navigation/>
+          <div className="sidebar__divider"></div>
+          <Filter/>
+        </div>
+      </div>
+    )
+  }
+} 
 
 export default Sidebar
