e6ef6a0187b18048b1ec30ab4b18c571f6ccb954
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-23 11:50:50 +0530

    refactored

--- src/components/Auth/Logout/Logout.jsx
+++ src/components/Auth/Logout/Logout.jsx
@@ -1,21 +1,24 @@
 import React, { Component } from 'react'
 import * as actions from '../../../store/actions/index'
-import {Redirect} from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 class Logout extends Component {
-    componentDidMount() {
-        this.props.onLogout(this.props.history)
-    }
+  componentDidMount() {
+    this.props.onLogout(this.props.history)
+  }
   render() {
     return <Redirect to="/" />
   }
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        onLogout: ()=> dispatch(actions.logout())
-    }
+  return {
+    onLogout: () => dispatch(actions.logout())
+  }
 }
 
-export default connect(null, mapDispatchToProps)(Logout)
+export default connect(
+  null,
+  mapDispatchToProps
+)(Logout)
