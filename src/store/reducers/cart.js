58d9510c5e4e5e6e259f2a60a6dd6729894c1a9e
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-23 12:10:42 +0530

    case added for remove product

--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -1,76 +1,109 @@
-import * as actionTypes from '../actions/actionTypes';
+import * as actionTypes from '../actions/actionTypes'
 
+// initial State for cart reducer
 const initialState = {
-    cart: [],
-    totalPrice: 0,
-    isCartActive: false,
-    checkout: false
+  cart: [],
+  totalPrice: 0,
+  isCartActive: false,
+  checkout: false
 }
 
 // initialize cart
 const cartInit = (state, action) => {
-    return initialState
+  return initialState
 }
 
 // add product to cart
 const addProductToCart = (state, action) => {
-    let product = action.product;
-    let cart = [...state.cart]
-    
-    // if product is in state
-    let tempProduct = state.cart.find(p => p.id === product.id)
-    if (tempProduct) {
-        let index = state.cart.indexOf(tempProduct)
-        let quantity = tempProduct.quantity + 1
+  let product = action.product
 
-        return { ...state,
-            cart: state.cart.slice(0, index)
-                .concat([{
-                    ...state.cart[index],
-                    quantity: quantity
-                }])
-                .concat(state.cart.slice(index + 1)),
-            totalPrice: state.totalPrice + product.price
-        }
-    }
+  // if product is in state
+  let tempProduct = state.cart.find(p => p.id === product.id)
+  if (tempProduct) {
+    let index = state.cart.indexOf(tempProduct)
+    let quantity = tempProduct.quantity + 1
 
-    // if product is not in state , store product with quantity = 1
-    else {
-        product.quantity = 1;
-        return { ...state,
-            cart: cart.concat(product),
-            totalPrice: state.totalPrice + product.price
-        }
+    // return state with updated product quantity -> product.quantity + 1
+    return {
+      ...state,
+      cart: state.cart
+        .slice(0, index)
+        .concat([
+          {
+            ...state.cart[index],
+            quantity: quantity
+          }
+        ])
+        .concat(state.cart.slice(index + 1)),
+      totalPrice: state.totalPrice + product.price
     }
+  }
+
+  // if product is not in state , store product with quantity = 1
+  else {
+    product.quantity = 1
+    return {
+      ...state,
+      cart: state.cart.concat(product),
+      totalPrice: state.totalPrice + product.price
+    }
+  }
 }
 
-// toggle cart state
-const toggleCart = (state, action) => {
-    return { ...state,
-        isCartActive: !state.isCartActive
+// subtract or remove product from cart
+const removeProductFromCart = (state, action) => {
+  let { product } = action
+
+  let tempProduct = state.cart.find(p => p.id === product.id)
+
+  tempProduct.quantity -= 1
+  let index = state.cart.indexOf(tempProduct)
+
+  // if quantity > 0, subtract quantity of product in cart
+  if (tempProduct.quantity > 0) {
+    return {
+      ...state,
+      cart: state.cart
+        .slice(0, index)
+        .concat(tempProduct)
+        .concat(state.cart.slice(index + 1)),
+      totalPrice: state.totalPrice - product.price
     }
+  } 
+  // if quantity = 0 , then remove product from cart
+  else {
+    return {
+      ...state,
+      cart: state.cart.slice(0, index).concat(state.cart.slice(index + 1)),
+      totalPrice: state.totalPrice - product.price
+    }
+  }
 }
 
+// method to set checkout
 const cartCheckout = (state, action) => {
-    return {...state,
-        checkout: true
-    }
+  return {
+    ...state,
+    checkout: true
+  }
 }
 
 // ---------------- CART reducer ------------------------
 export default (state = initialState, action) => {
-    switch (action.type) {
+  switch (action.type) {
+    case actionTypes.CART_INIT:
+      return cartInit(state, action)
 
-        case actionTypes.CART_INIT: 
-            return cartInit(state, action)
-        case actionTypes.ADD_PRODUCT_TO_CART:
-            return addProductToCart(state, action)
-        case actionTypes.TOGGLE_CART:
-            return toggleCart(state, action)
-        case actionTypes.CART_CHECKOUT:
-            return cartCheckout(state, action)
+    case actionTypes.ADD_PRODUCT_TO_CART:
+      return addProductToCart(state, action)
 
-        default:
-            return state
-    }
+    case actionTypes.REMOVE_PRODUCT_FROM_CART:
+      return removeProductFromCart(state, action)
+
+    case actionTypes.CART_CHECKOUT:
+      return cartCheckout(state, action)
+
+    default:
+      return state
+  }
 }
